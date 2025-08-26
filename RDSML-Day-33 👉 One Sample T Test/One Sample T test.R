867a2d112988026202746f7b91688697fae39fc7
Kaifi Sultana Kabbo <85066579+Kaifi-Sultana@users.noreply.github.com>
2025-08-26 19:48:57 +0600

    Files updated.

--- RDSML-Day-33 👉 One Sample T Test/One Sample T test.R
+++ RDSML-Day-33 👉 One Sample T Test/One Sample T test.R
@@ -1,5 +1,7 @@
 # If population mean and population sd is not given,
 # One sample Z test cannot be done. 
+# When population sd is not given, one sample T test can be done.
+# Data must be in normal distribution to conduct all T tests.
 
 # Importing the dataset
 myData <- read.csv("bottle_data.csv")
